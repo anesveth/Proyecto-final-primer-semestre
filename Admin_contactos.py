ab81fd4fda2b7cece36e61639af044213956dd02
Jean <jpmejicanos@ufm.edu>
2019-05-08 07:10:24 -0600

    pretty print

--- Admin_contactos.py
+++ Admin_contactos.py
@@ -2,8 +2,12 @@
 import request
 import os
 
+
 #Controls program logic
 def main():
+    #lista
+    directorio =[{"Nombre":"asd", "Apellido":"Tomas", "Telefono":"443217"}]
+    #opciones
     print("Que quieres hacer?")
     print("1 - añadir contacto\n2 - remover contacto\n3 - Mostrar contactos")
     Answer = input()
@@ -11,27 +15,26 @@ def main():
         name = input("Ingresa el nombre: ")
         srname = input("Ingresa el apellido: ")
         phone = input("Ingresa el numero telefónico: ")
-        add_contact(name, srname, phone)
+        add_contact(directorio, name, srname, phone)
     elif Answer == 2:
         remove_contact()
     elif Answer == 3:
         pretty_print()
-#pasa los contactos
-def get_contacts_list():
-    ls =[{"Nombre":"asd", "Apellido":"Tomas", "Telefono":"443217"}]
-    return ls
 
 #adds a contact
-def add_contact(name, srname, phone):
-    pass
+def add_contact(directorio, name, srname, phone):
+    clean()
+
+
 
 #removes a contact
 def remove_contact():
     pass
 
 #se imprime de una buena manera
-def pretty_print():
-    pass
+def pretty_print(directorio):
+    for contacto in directorio:
+        print("") 
 
 
 
